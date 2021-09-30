b529418c3a61f039c244026c047fbd0759c7698f
kanakbabar <kanakbabar999@gamil.com>
2021-09-30 21:35:50 +0530

    [kanak]Adress Book

---
+++ program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AdressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            uc1_CreatContact.CC();
+            uc2_Add.AddNewContact();
+            uc3_Edit.Edit();
+            uc4_Delete.Delete();
+            Day10uc_5.uc5();
+            Console.ReadLine();
+
+
+        }
+          
+    }
+}
